6e314e37c2387aa006992bdf19862ce0bb9b1973
TY-089\Administrator <lmw6412036@sina.com>
2017-09-14 17:57:51 +0800

    Changes to be committed: 	modified:   index.html 	modified:   src/lib/api.js 	new file:   src/lib/cache.js 	new file:   src/lib/config.js 	modified:   src/main.js 	new file:   static/js/url.min.js

--- src/main.js
+++ src/main.js
@@ -5,18 +5,8 @@ import App from './App'
 import router from './router'
 import Vuelidate from 'vuelidate'
 
+import {openidCache} from "./lib/cache"
 
-Vue.use(Vuelidate)
-Vue.config.productionTip = false
-// Vue.mixin({
-//   created: function () {
-//     var myOption = this.$options.myOption
-//     var token = localStorage.getItem("token")
-//     if (!token) {
-//      this.$router.push('/login')
-//     }
-//   }
-// })
 let UA = window.navigator.userAgent.toLocaleLowerCase();
 if (/iphone/.test(UA)) {
   window.device = "iphone";
@@ -30,19 +20,54 @@ if (/micromessenger/.test(UA)) {
 } else {
   window.brower = "";
 }
+
+
+Vue.use(Vuelidate)
+Vue.config.productionTip = false
+// Vue.mixin({
+//   created: function () {
+//     var myOption = this.$options.myOption
+//     var token = localStorage.getItem("token")
+//     if (!token) {
+//      this.$router.push('/login')
+//     }
+//   }
+// })
+
 /*
  * 移动端rem布局
  * width:20rem
  * */
 if (window.device == "iphone" || window.device == "android") {
   let w = window.innerWidth;
-  document.querySelector('html').style.fontSize = `${w/ 20}px`;
+  document.querySelector('html').style.fontSize = `${w / 20}px`;
 }
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App },
-  myOption: 'hello!'
-})
+
+
+let openid = openidCache.get();
+let params = url("?");
+let re = encodeURIComponent(location.href);
+if (window.brower == "weixin") {
+  if (openid) {
+    init();
+  } else if (!openid && !params.code && !params.state) {
+    let url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx6e3551dee5f7e716&redirect_uri=${re}&response_type=code&scope=snsapi_userinfo&state=STATE&component_appid=wxe24cab30ebb1e366#wechat_redirect`;
+    location.href = url;
+  } else if (!openid && params.code && params.state) {
+
+  }
+} else {
+  init();
+}
+
+
+function init() {
+  /* eslint-disable no-new */
+  new Vue({
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: {App},
+    myOption: 'hello!'
+  });
+}
