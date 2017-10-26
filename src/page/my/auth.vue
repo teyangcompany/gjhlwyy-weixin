be0f4c3592f7c036fe9fb64dfba3b9d71a0afc89
TY-089\Administrator <lmw6412036@sina.com>
2017-10-26 17:48:34 +0800

    Changes to be committed: 	modified:   index.ejs 	modified:   package.json 	modified:   src/components/index/book/bookTerms.vue 	modified:   src/components/index/onlineDoctor/internetRoom.vue 	modified:   src/components/index/onlineDoctor/onlineDoctorCard.vue 	modified:   src/components/my/usualPatient.vue 	modified:   src/lib/filter.js 	modified:   src/main.js 	new file:   src/page/my/auth.vue 	modified:   src/page/my/data.vue 	modified:   src/plugins/app-header.vue 	new file:   static/img/logo.web.png 	new file:   static/img/logo.weixin.png

---
+++ src/page/my/auth.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page">
+    <app-header title="实名认证"></app-header>
+  </div>
+</template>
+
+<script>
+  import AppHeader from "../../plugins/app-header.vue"
+
+  export default {
+    data() {
+      return {};
+    },
+    computed: {},
+    components: {AppHeader},
+    mounted() {
+
+    },
+    beforeDestroy() {
+
+    },
+    methods: {}
+  };
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/plugins/app-header.vue
+++ src/plugins/app-header.vue
@@ -1,68 +1,68 @@
 <template>
-    <div class="header relative center">
-        <div class="back absolute" @click="$router.go(-1)">
-            <slot name="back"></slot>
-        </div>
-        <slot name="right"></slot>
-        <span>{{title}}</span>
+  <div class="header relative center">
+    <div class="back absolute" @click.stop="$router.go(-1)">
+      <slot name="back"></slot>
     </div>
+    <slot name="right"></slot>
+    <span>{{title}}</span>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            title: {
-                type: String,
-                default: ""
-            }
-        },
-        data() {
-            return {};
-        },
-        computed: {},
-        components: {},
-        mounted() {
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: ""
+      }
+    },
+    data() {
+      return {};
+    },
+    computed: {},
+    components: {},
+    mounted() {
 
-        },
-        beforeDestroy() {
+    },
+    beforeDestroy() {
 
-        },
-        methods: {}
-    };
+    },
+    methods: {}
+  };
 </script>
 
 <style scoped lang="scss">
-    @import "../common/var";
-    @import "../common/mixin";
+  @import "../common/var";
+  @import "../common/mixin";
 
-    %a {
-        line-height: 45px; /*no*/
+  %a {
+    line-height: 45px; /*no*/
+    height: 45px; /*no*/
+  }
+
+  .header {
+    font-size: 18px; /*no*/
+    background-color: white;
+    @include border(bottom);
+    @extend %a;
+
+    .back {
+      @include t_r_b_l(0, 1, 1, 0);
+      i {
+        display: block;
+        width: 45px; /*no*/
         height: 45px; /*no*/
+        background-image: url(../../static/img/返回.png);
+        @include backgroundImageSet(21px/2, 34px/2, $commonSpace);
+      }
     }
 
-    .header {
-        font-size: 18px; /*no*/
-        background-color: white;
-        @include border(bottom);
-        @extend %a;
-
-        .back {
-            @include t_r_b_l(0, 1, 1, 0);
-            i {
-                display: block;
-                width: 45px; /*no*/
-                height: 45px; /*no*/
-                background-image: url(../../static/img/返回.png);
-                @include backgroundImageSet(21px/2, 34px/2, $commonSpace);
-            }
-        }
-
-        .right {
-            right: $commonSpace;
-            top: 0px;
-            @extend %a;
-            font-size: 16px; /*no*/
-            color: $mainColor;
-        }
+    .right {
+      right: $commonSpace;
+      top: 0px;
+      @extend %a;
+      font-size: 16px; /*no*/
+      color: $mainColor;
     }
+  }
 </style>
