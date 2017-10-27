b8bacb6998d6f2ce5dd750c4e58e6bbcd9401bb5
TY-089\Administrator <lmw6412036@sina.com>
2017-10-27 18:59:35 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   package.json
    	modified:   src/page/my/auth.vue

--- src/page/my/auth.vue
+++ src/page/my/auth.vue
@@ -1,30 +1,91 @@
 <template>
-    <div class="page">
-        <app-header title="实名认证">
-            <i slot="back"></i>
-        </app-header>
+  <div class="auth page flex">
+    <app-header title="实名认证" class="flex0">
+      <i slot="back"></i>
+    </app-header>
+    <div class="main">
+      <ul>
+        <li class="flex">
+          <div class="icon flex0"><img src="../../../static/img/yy.auth.png" alt=""></div>
+          <div class="text flex1">
+            <div class="name">医院认证</div>
+            <div class="desc">绑定医院账号即完成认证</div>
+          </div>
+          <div class="right flex0">
+            <span>点击认证</span>
+          </div>
+        </li>
+        <li class="flex">
+          <div class="icon flex0"><img src="../../../static/img/ga.auth.png" alt=""></div>
+          <div class="text flex1">
+            <div class="name">公安认证</div>
+            <div class="desc">由公安系统对您的姓名、<br>身份证号进行认证</div>
+          </div>
+          <div class="right flex0"></div>
+        </li>
+        <li class="flex">
+          <div class="icon flex0"><img src="../../../static/img/yys.auth.png" alt=""></div>
+          <div class="text flex1">
+            <div class="name">运营商认证</div>
+            <div class="desc">由运营商对您的姓名、<br>身份证号、手机号进行认证</div>
+          </div>
+          <div class="right flex0"></div>
+        </li>
+      </ul>
     </div>
+
+  </div>
 </template>
 
 <script>
-    import AppHeader from "../../plugins/app-header.vue"
+  import AppHeader from "../../plugins/app-header.vue"
 
-    export default {
-        data() {
-            return {};
-        },
-        computed: {},
-        components: {AppHeader},
-        mounted() {
+  export default {
+    data() {
+      return {};
+    },
+    computed: {},
+    components: {AppHeader},
+    mounted() {
 
-        },
-        beforeDestroy() {
+    },
+    beforeDestroy() {
 
-        },
-        methods: {}
-    };
+    },
+    methods: {}
+  };
 </script>
 
 <style scoped lang="scss">
+  @import "../../common/public";
 
+  .auth {
+    flex-direction: column;
+    .main {
+      li + li {
+        @include border(top);
+      }
+      li {
+        padding: 10px;
+        align-items: center;
+        .icon {
+          margin-right: 10px;
+          img {
+            width: 60px;
+          }
+        }
+        .text {
+          .name {
+            font-size: 16px;
+          }
+          .desc {
+            line-height: 1.3;
+            padding-top: 5px;
+            color: #999999;
+            font-size: 12px;
+          }
+        }
+      }
+    }
+  }
 </style>
