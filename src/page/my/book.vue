8f2db4776d83f2ae046a0c98e40912e8a11f30c9
lmw6412036 <lmw6412036@sina.com>
2018-01-16 23:02:19 +0800

    1

--- src/page/my/book.vue
+++ src/page/my/book.vue
@@ -1,35 +1,41 @@
 <template>
-  <div class="page">
-    <app-header title="我的挂号" ref="header">
-      <i slot="back" class="back"></i>
-    </app-header>
-    <scroll :height="scrollHeight" class="main overflow-hidden" :data="list">
-      <ul>
-        <li v-for="(data,index) in list" :key="index">
-          <h3 class="flex">
-            <div class="title flex1">{{data.deptName}} {{data.docName}}</div>
-            <div class="status flex0" :class="['status'+data.orderStatus]">{{data.orderStatus|getOrderStatus}}</div>
-          </h3>
-          <div class="time">
-            {{data.bookTime}} {{data.bookAmpm|getOrderAmpm}} {{data.bookNo}}号
-          </div>
-          <div class="hos">
-            {{data.hosName}}
-          </div>
-          <div class="pat">
-            就诊人:{{data.compatName}}
-          </div>
-          <div class="fee">
-            挂号费:￥{{data.bookFee}}
-          </div>
+    <div class="page">
+        <app-header title="我的挂号" ref="header">
+            <i slot="back" class="back"></i>
+        </app-header>
+        <scroll :height="scrollHeight" class="main overflow-hidden" :data="list">
+            <ul>
+                <div v-if="list.length==0" class="nodata center pt500 fs46">
+                    暂无挂号记录 <br>
+                    <router-link to="/bookTerms">去挂号</router-link>
+                </div>
+                <li v-for="(data,index) in list" :key="index">
+                    <h3 class="flex">
+                        <div class="title flex1">{{data.deptName}} {{data.docName}}</div>
+                        <div class="status flex0" :class="['status'+data.orderStatus]">
+                            {{data.orderStatus|getOrderStatus}}
+                        </div>
+                    </h3>
+                    <div class="time">
+                        {{data.bookTime}} {{data.bookAmpm|getOrderAmpm}} {{data.bookNo}}号
+                    </div>
+                    <div class="hos">
+                        {{data.hosName}}
+                    </div>
+                    <div class="pat">
+                        就诊人:{{data.compatName}}
+                    </div>
+                    <div class="fee">
+                        挂号费:￥{{data.bookFee}}
+                    </div>
 
-          <div class="bottom right" v-if="data.orderStatus==0||data.orderStatus==1">
-            <span v-if="data.orderStatus==1" @click="handleOrder('cancel',data)">取消</span>
-          </div>
-        </li>
-      </ul>
-    </scroll>
-  </div>
+                    <div class="bottom right" v-if="data.orderStatus==0||data.orderStatus==1">
+                        <span v-if="data.orderStatus==1" @click="handleOrder('cancel',data)">取消</span>
+                    </div>
+                </li>
+            </ul>
+        </scroll>
+    </div>
 </template>
 
 <script>
@@ -103,63 +109,71 @@
 </script>
 
 <style scoped lang="scss">
-  @import "../../common/public";
+    @import "../../common/public";
 
-  .main {
-    background-color: #F8F8F8;
-    ul {
-      padding-top: $commonSpace;
+    .pt500{
+        margin-top: px2rem(512px,1080);
     }
-    li + li {
-      margin-top: $commonSpace;
+
+    .fs46{
+        font-size: px2rem(46px,1080);
     }
-    li {
-      border-radius: 10px;
-      margin-left: $commonSpace;
-      margin-right: $commonSpace;
-      padding: $commonSpace;
-      background-color: white;
-      line-height: 1;
-      h3 {
-        font-size: px2rem(46px, 1080);
-        color: #333333;
-        .status {
-          font-weight: bold;
-          color: $mainColor;
-          &.status5 {
-            color: #A1A1A1;
-          }
+
+    .main {
+        background-color: #F8F8F8;
+        ul {
+            padding-top: $commonSpace;
         }
-      }
-      .time {
-        margin-top: px2rem(46px, 1080);
-        font-size: px2rem(40px, 1080);
-        color: #333333;
-      }
-      .hos {
-        margin-top: px2rem(60px, 1080);
-      }
-      .pat, .fee, .bottom {
-        margin-top: px2rem(42px, 1080);
-      }
-      .hos, .pat, .fee {
-        font-size: px2rem(40px, 1080);
-        color: #999999;
-      }
-      .bottom {
-        margin-left: 0 - $commonSpace;
-        margin-right: 0 - $commonSpace;
-        @include border(top);
-        padding-top: $commonSpace;
-        padding-right: $commonSpace;
-        span {
-          font-size: px2rem(40px, 1080);
-          color: #A1A1A1;
-          border: 1px solid #A1A1A1;
-          padding: 2px 7px;
-          border-radius: 5px;
+        li + li {
+            margin-top: $commonSpace;
         }
-      }
+        li {
+            border-radius: 10px;
+            margin-left: $commonSpace;
+            margin-right: $commonSpace;
+            padding: $commonSpace;
+            background-color: white;
+            line-height: 1;
+            h3 {
+                font-size: px2rem(46px, 1080);
+                color: #333333;
+                .status {
+                    font-weight: bold;
+                    color: $mainColor;
+                    &.status5 {
+                        color: #A1A1A1;
+                    }
+                }
+            }
+            .time {
+                margin-top: px2rem(46px, 1080);
+                font-size: px2rem(40px, 1080);
+                color: #333333;
+            }
+            .hos {
+                margin-top: px2rem(60px, 1080);
+            }
+            .pat, .fee, .bottom {
+                margin-top: px2rem(42px, 1080);
+            }
+            .hos, .pat, .fee {
+                font-size: px2rem(40px, 1080);
+                color: #999999;
+            }
+            .bottom {
+                margin-left: 0 - $commonSpace;
+                margin-right: 0 - $commonSpace;
+                @include border(top);
+                padding-top: $commonSpace;
+                padding-right: $commonSpace;
+                span {
+                    font-size: px2rem(40px, 1080);
+                    color: #A1A1A1;
+                    border: 1px solid #A1A1A1;
+                    padding: 2px 7px;
+                    border-radius: 5px;
+                }
+            }
+        }
     }
-  }
 </style>
