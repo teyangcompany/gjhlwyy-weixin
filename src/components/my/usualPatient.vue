f87bf3908e81fbf7fee6923a6ae2a19da75cf662
lmw6412036 <lmw6412036@sina.com>
2018-03-04 12:02:33 +0800

    1

--- src/components/my/usualPatient.vue
+++ src/components/my/usualPatient.vue
@@ -1,30 +1,30 @@
 <template>
-  <transition name="slide">
-    <div class="wrapWhole">
-      <v-header :title="title" :rightTitle="rightTitle" @0n-addUsual="addPatient()"
-                @on-forceMy="goForceMy()"></v-header>
-      <div class="usual" ref="usual">
-        <div>
-          <router-link tag="div" :to="{path:'/detailUsual',query:{index:index}}" class="usualLine"
-                       v-for="(item,index) in patientList" :key="item.id">
-            <div class="usualCenter">
-              <ul>
-                <li><span class="patientName">{{ item.compatName }}</span><span>{{ item.compatAge
+    <transition name="slide">
+        <div class="wrapWhole">
+            <v-header :title="title" :rightTitle="rightTitle" @0n-addUsual="addPatient()"
+                      @on-forceMy="goForceMy()"></v-header>
+            <div class="usual" ref="usual">
+                <div>
+                    <router-link tag="div" :to="{path:'/detailUsual',query:{index:index}}" class="usualLine"
+                                 v-for="(item,index) in patientList" :key="item.id">
+                        <div class="usualCenter">
+                            <ul>
+                                <li><span class="patientName">{{ item.compatName }}</span><span>{{ item.compatAge
                   }} &nbsp;{{ item.compatGender == "M" ? "男" : "女"}}</span> <span
-                  :class="[item.isRealnameAuth?'ok':'no']">{{item.isRealnameAuth | isAuth}}</span>
-                </li>
-                <li>身份证号：<span>{{item.compatIdcard|formatCardAndMobile(3,4,12)}}</span>
-                </li>
-                <li>手机号：<span>{{ item.compatMobile|formatCardAndMobile(3,4,4)}}</span></li>
-                <li v-if="!(item.compatMedicalRecord)">病案号：<span>暂未绑定病案号</span></li>
-                <li v-else>病案号：<span>{{ item.compatMedicalRecord }}</span></li>
-              </ul>
+                                        :class="[item.isRealnameAuth?'ok':'no']">{{item.isRealnameAuth | isAuth}}</span>
+                                </li>
+                                <li>身份证号：<span>{{item.compatIdcard|formatCardAndMobile(3,4,12)}}</span>
+                                </li>
+                                <li>手机号：<span>{{ item.compatMobile|formatCardAndMobile(3,4,4)}}</span></li>
+                                <li v-if="!(item.compatMedicalRecord)">病案号：<span>暂未绑定病案号</span></li>
+                                <li v-else>病案号：<span>{{ item.compatMedicalRecord }}</span></li>
+                            </ul>
+                        </div>
+                    </router-link>
+                </div>
             </div>
-          </router-link>
         </div>
-      </div>
-    </div>
-  </transition>
+    </transition>
 </template>
 <script>
   import {isAuth, formatCardAndMobile} from "../../lib/filter"
@@ -43,18 +43,7 @@
       }
     },
     created() {
-      api("nethos.pat.compat.list", {
-        token: tokenCache.get()
-      }).then((data) => {
-        if (data.code == 0) {
-          this.patientList = data.list
-          console.log(this.patientList)
-        } else if (!(data.msg)) {
-          weui.alert("网络错误，请稍后重试")
-        } else {
-          weui.alert(data.msg)
-        }
-      })
+      this.$router.replace('/my/patient')
     },
     methods: {
       addPatient() {
@@ -85,79 +74,79 @@
   }
 </script>
 <style scoped lang="scss">
-  @import '../../common/public.scss';
+    @import '../../common/public.scss';
 
-  .wrapWhole {
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-  }
-
-  .usual {
-    position: fixed;
-    top: 50px;
-    left: 0;
-    right: 0;
-    bottom: 20px;
-    background-color: white;
-    .usualLine {
-      width: 100%;
-      background-color: white;
+    .wrapWhole {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
     }
-    .usualCenter {
-      width: 690rem/$rem;
-      margin: 0 auto;
-      /*margin-top: 5px;*/
-      background-color: white;
-      ul {
-        margin: 0;
-        padding: 0;
-        margin-top: 10px;
-        li {
-          list-style-type: none;
-          height: 80rem/$rem;
-          line-height: 80rem/$rem;
-          margin-top: 1px;
-          background-color: $bgColor2;
-          font-size: 32rem/$rem;
-          padding-left: 10px;
-          span {
-            font-size: 32rem/$rem;
-            color: #999999;
-            &.no {
-              color: red;
+
+    .usual {
+        position: fixed;
+        top: 50px;
+        left: 0;
+        right: 0;
+        bottom: 20px;
+        background-color: white;
+        .usualLine {
+            width: 100%;
+            background-color: white;
+        }
+        .usualCenter {
+            width: 690rem/$rem;
+            margin: 0 auto;
+            /*margin-top: 5px;*/
+            background-color: white;
+            ul {
+                margin: 0;
+                padding: 0;
+                margin-top: 10px;
+                li {
+                    list-style-type: none;
+                    height: 80rem/$rem;
+                    line-height: 80rem/$rem;
+                    margin-top: 1px;
+                    background-color: $bgColor2;
+                    font-size: 32rem/$rem;
+                    padding-left: 10px;
+                    span {
+                        font-size: 32rem/$rem;
+                        color: #999999;
+                        &.no {
+                            color: red;
+                        }
+                        &.ok {
+                            color: $mainColor;
+                        }
+                    }
+                    .patientName {
+                        color: #333333;
+                        display: inline-block;
+                        min-width: 150rem/$rem;
+                    }
+                }
+                li:first-child {
+                    border-top-left-radius: 7px;
+                    border-top-right-radius: 7px;
+                }
+                li:last-child {
+                    border-bottom-left-radius: 7px;
+                    border-bottom-right-radius: 7px;
+                }
             }
-            &.ok {
-              color: $mainColor;
-            }
-          }
-          .patientName {
-            color: #333333;
-            display: inline-block;
-            min-width: 150rem/$rem;
-          }
         }
-        li:first-child {
-          border-top-left-radius: 7px;
-          border-top-right-radius: 7px;
-        }
-        li:last-child {
-          border-bottom-left-radius: 7px;
-          border-bottom-right-radius: 7px;
-        }
-      }
     }
-  }
 
-  .slide-enter-active, .slide-leave-active {
-    transition: all 0.3s;
-    opacity: 1;
-  }
+    .slide-enter-active, .slide-leave-active {
+        transition: all 0.3s;
+        opacity: 1;
+    }
 
-  .slide-enter, .slide-leave-to {
-    transform: translate3d(100%, 0, 0);
-    opacity: 1;
-  }
+    .slide-enter, .slide-leave-to {
+        transform: translate3d(100%, 0, 0);
+        opacity: 1;
+    }
 </style>
--- src/page/my/patient.vue
+++ src/page/my/patient.vue
@@ -1,41 +1,42 @@
 <template>
-  <div class="page bg">
-    <app-header title="常用就诊人">
-      <i slot="back" class="back"></i>
-      <div class="right absolute" slot="right">添加</div>
-    </app-header>
-    <scroll :height="scrollHeight" :data="list">
-      <ul>
-        <li class="flex" v-for="i in 5">
-          <div class="flex1 lh1">
-            <h3 class="flex">
-              <div class="name fs40 color_333">李明卫</div>
-              <div class="sex fs35 color_666">男</div>
-              <div class="age fs35 color_666">32</div>
-              <div class="status">
-                <span class="fs30 color_999">已认证</span>
-              </div>
-            </h3>
-            <div class="idcard flex fs35">
-              <my-label class="flex0 color_999" title="身份证号"></my-label>
-              <span class="flex1 color_777 fs35">92938393393</span>
-            </div>
-            <div class="phone flex fs35">
-              <my-label class="flex0 color_999" title="手机号码"></my-label>
-              <span class="flex1 color_777 fs35">92938393393</span>
-            </div>
-            <div class="medical flex fs35">
-              <my-label class="flex0 color_999" title="病案号"></my-label>
-              <span class="flex1 color_777 fs35">92938393393</span>
-            </div>
-          </div>
-          <div class="flex0 icon">
-            <img src="../../../static/img/arrow.png" alt="">
-          </div>
-        </li>
-      </ul>
-    </scroll>
-  </div>
+    <div class="page bg">
+        <app-header title="常用就诊人">
+            <i slot="back" class="back"></i>
+            <router-link to="/addUsualPatient" tag="div" class="right absolute" slot="right">添加</router-link>
+        </app-header>
+        <scroll :height="scrollHeight" class="overflow-hidden" :data="list">
+            <ul>
+                <router-link :key="index" tag="li" :to="{path:'/detailUsual',query:{index:index}}" class="flex"
+                             v-for="(item,index) in list">
+                    <div class="flex1 lh1">
+                        <h3 class="flex">
+                            <div class="name fs40 mr30 color_333">{{item.compatName}}</div>
+                            <div class="sex mr30 fs35 color_666">{{item.compatGender|getGender}}</div>
+                            <div class="age mr30 fs35 color_666">{{item.compatAge}}</div>
+                            <div class="status">
+                                <span class="block fs30 color_999" :class="['status'+item.isRealnameAuth]">{{item.isRealnameAuth|isAuth}}</span>
+                            </div>
+                        </h3>
+                        <div class="idcard flex fs35">
+                            <my-label class="flex0 color_999" title="身份证号"></my-label>
+                            <span class="flex1 color_777 fs35">{{item.compatIdcard|formatCardAndMobile(3,4,11)}}</span>
+                        </div>
+                        <div class="phone flex fs35">
+                            <my-label class="flex0 color_999" title="手机号码"></my-label>
+                            <span class="flex1 color_777 fs35">{{item.compatMobile|formatCardAndMobile(3,4,4)}}</span>
+                        </div>
+                        <div class="medical flex fs35">
+                            <my-label class="flex0 color_999" title="病案号"></my-label>
+                            <span class="flex1 color_777 fs35">{{item.compatMedicalRecord||'暂无病案号'}}</span>
+                        </div>
+                    </div>
+                    <div class="flex0 icon">
+                        <img src="../../../static/img/arrow.png" alt="">
+                    </div>
+                </router-link>
+            </ul>
+        </scroll>
+    </div>
 </template>
 
 <script>
@@ -43,6 +44,9 @@
   import {scrollHeightMixin} from "../../lib/mixin";
   import Scroll from '../../plugins/scroll'
   import MyLabel from '../../plugins/my-label'
+  import api from '../../lib/api'
+  import weuijs from 'weui.js'
+  import {getGender, isAuth, formatCardAndMobile} from "../../lib/filter";
 
   export default {
     data() {
@@ -51,55 +55,98 @@
       };
     },
     computed: {},
+    filters: {getGender, isAuth, formatCardAndMobile},
     components: {AppHeader, Scroll, MyLabel},
     mixins: [scrollHeightMixin],
+    created() {
+      this.getList();
+    },
+
     mounted() {
 
     },
     beforeDestroy() {
 
     },
-    methods: {}
+    methods: {
+      async getList() {
+        let loading = weuijs.loading("加载中...");
+        let ret = await api('nethos.pat.compat.list', {});
+        loading.hide();
+        if (ret.code == 0) {
+          this.list = ret.list;
+        } else {
+          //this.$refs.msg.show(ret.msg||"错误代码"+ret.code);
+        }
+      }
+    }
   };
 </script>
 
 <style scoped lang="scss">
-  @import "../../common/public";
+    @import "../../common/public";
 
-  .fs40 {
-    font-size: 16px;
-  }
-
-  .fs35 {
-    font-size: 14px;
-  }
-
-  .fs30 {
-    font-size: 12px;
-  }
-
-  li {
-    background-color: white;
-    padding: px2rem(60px, 1080) px2rem(40px, 1080);
-
-    align-items: center;
-    .my-label {
-      margin-right: px2rem(30px, 1080);
-      width: px2rem(65px, 375);
-      justify-content: space-between;
+    .fs40 {
+        font-size: 16px;
     }
 
-    .idcard, .phone, .medical {
-      margin-top: px2rem(57px, 1080);
+    .fs35 {
+        font-size: 14px;
     }
-  }
 
-  li + li {
-    margin-top: px2rem(27px, 1080);
-  }
+    .fs30 {
+        font-size: 12px;
+    }
 
-  ul {
-    padding-top: px2rem(27px, 1080);
-  }
+    .mr30 {
+        margin-right: px2rem(30px, 1080);
+    }
+
+    h3 {
+        align-items: center;
+        height: 20px;
+    }
+
+    .status {
+        .statusfalse {
+            border: 1px solid red;
+            color: red;
+        }
+        span {
+            border: 1px solid #999999;
+            padding: 2px 5px;
+            border-radius: 3px;
+        }
+    }
+
+    .icon {
+        img {
+            width: px2rem(17px, 750);
+        }
+    }
+
+    li {
+        background-color: white;
+        padding: px2rem(60px, 1080) px2rem(40px, 1080);
+
+        align-items: center;
+        .my-label {
+            margin-right: px2rem(30px, 1080);
+            width: px2rem(65px, 375);
+            justify-content: space-between;
+        }
+
+        .idcard, .phone, .medical {
+            margin-top: px2rem(57px, 1080);
+        }
+    }
+
+    li + li {
+        margin-top: px2rem(27px, 1080);
+    }
+
+    ul {
+        padding-top: px2rem(27px, 1080);
+    }
 
 </style>
